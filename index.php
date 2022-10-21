e8d39435088079e466999c018a69cbe5e7ba444d
Michiel de Jong <michiel@unhosted.org>
2022-10-21 14:26:42 +0200

    p12transport

--- index.php
+++ index.php
@@ -30,20 +30,31 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 <html>
   <head>
     <title>Connect your Books!</title>
+    <style>
+      body { background-color: #A3DDDF }
+      header { width: 40em; margin: 5em auto; color: white }
+      article { width: 40em; margin: 5em auto; padding: 3em; border-radius: 1em; background-color: #FFE4B5 }
+      article pre { background-color: grey; padding: 3em; color: yellow; border-radius: 1em; text-align: center }
+      footer { width: 40em; margin: 5em auto; color: white }
+    </style>
   </head>
   <body>
-    <h2>Connect your Books!</h2>
-    <p>We at <a href="https://pondersource.com">Ponder Source</a> are working on a product that will allow bookkeeping systems to be connected
+    <header>
+      <h2>Connect your Books!</h2>
+    </header>
+    <article>
+      <p>We at <a href="https://pondersource.com">Ponder Source</a> are working on a product that will allow bookkeeping systems to be connected
       to each other, in the vision of <a href="https://federatedbookkeeping.org">Federated Bookkeeping</a>.</p>
-    <p>For this, we will combine a number of technologies:</p>
-    <ul>
-      <li><a href="https://peppol.eu">Peppol</a> and <a href="https://github.com/pondersource/peppol-php/blob/main/docs/ngi-assure-proposal.md">AS4-Direct</a> for e-invoicing</li>
-      <li><a href="https://www.w3.org/community/timesh/">Federated Timesheets</a></li>
-      <li>Integration of all the <a href="https://github.com/federatedbookkeeping/research/blob/main/api.md">proprietary billing APIs</a> we can find</li>
-      <li>Integration of all the bookkeeping software platforms we can find</li>
-      <li>Novel <a href="https://prejournal.org">Prejournal</a> bookkeeping techniques</li>
-    </ul>
-    <p>The HTML code of this website is maintained through a <a href="https://github.com/pondersource/connectyourbooks.com">GitHub repo</a>.</p>
+      <p>For this, we will combine a number of technologies:</p>
+      <ul>
+        <li><a href="https://peppol.eu">Peppol</a> and <a href="https://github.com/pondersource/peppol-php/blob/main/docs/ngi-assure-proposal.md">AS4-Direct</a> for e-invoicing</li>
+        <li><a href="https://www.w3.org/community/timesh/">Federated Timesheets</a></li>
+        <li>Integration of all the <a href="https://github.com/federatedbookkeeping/research/blob/main/api.md">proprietary billing APIs</a> we can find</li>
+        <li>Integration of all the bookkeeping software platforms we can find</li>
+        <li>Novel <a href="https://prejournal.org">Prejournal</a> bookkeeping techniques</li>
+      </ul>
+      <p>The HTML code of this website is maintained through a <a href="https://github.com/pondersource/connectyourbooks.com">GitHub repo</a>.</p>
+    </article>
   </body>
 </html>
 <?php
